590738fd9994d91501dc134520853eeeb5038925
4332001876 <mzfslhm@163.com>
2023-12-20 15:47:33 +0800

    dp-finished

--- Lab/Lab1/Data_Privacy_Lab1.md
+++ Lab/Lab1/Data_Privacy_Lab1.md
@@ -9,8 +9,22 @@
 本次实验的目的是使用DP-SGD算法训练一个二分类器，使得该二分类器能够对新的样本进行预测，判断该样本是否为恶性肿瘤。
 
 
+### 实验框架debug
+代码中的梯度下降公式有误，应改为如下形式（第一条公式可选择下面CrossEntropyLoss与L2Loss中的一种）：
+```python
+dz = -(y / (predictions + self.tau) - (1 - y) / (1 - predictions + self.tau)) # Cross entropy loss
+# dz = predictions - y # L2_loss
+dz = dz * (predictions * (1 - predictions)) # sigmoid derivative
 
+dw = np.dot(X.T, dz) / num_samples
+db = np.sum(dz) / num_samples
+```
 
+同时数据集的输入没有归一化，各输入特征的取值范围差异巨大（至少差4个数量级），导致训练过程中的动力学特性非常差，因此需要加入归一化。
+```python
+# normalize the data
+X = (X - np.mean(X, axis=0)) / X.std(axis=0)
+```
 
 
 
--- Lab/Lab1/dp-sgd.py
+++ Lab/Lab1/dp-sgd.py
@@ -10,7 +10,7 @@ class LogLevel:
     WARNING = 2
     ERROR = 3
     FATAL = 4
-LOG_LEVEL = LogLevel.DEBUG
+LOG_LEVEL = LogLevel.INFO
 
 def clip(x, lower_bound, upper_bound):
     return np.minimum(np.maximum(x, lower_bound), upper_bound)
@@ -29,8 +29,9 @@ class LogisticRegressionCustom:
     def __fit(self, X, y, is_dp=False, epsilon=None, delta=None, C=1):
         # Initialize weights and bias
         num_samples, num_features = X.shape
-        kaiming_uniform_bound = np.sqrt(3.0) / np.sqrt(num_features) # kaiming_uniform
-        self.weights = np.random.uniform(-kaiming_uniform_bound, kaiming_uniform_bound, size=(num_features,))
+        # kaiming_uniform_bound = np.sqrt(3.0) / np.sqrt(num_features) # kaiming_uniform
+        # self.weights = np.random.uniform(-kaiming_uniform_bound, kaiming_uniform_bound, size=(num_features,))
+        self.weights = np.zeros(num_features)
         self.bias = 0.0
 
         # Gradient descent optimization
@@ -52,7 +53,7 @@ class LogisticRegressionCustom:
             dw = np.dot(X.T, dz) / num_samples
             db = np.sum(dz) / num_samples
 
-            if LOG_LEVEL <= LogLevel.DEBUG and epoch % 200 == 1:
+            if LOG_LEVEL <= LogLevel.DEBUG and epoch % 200 == 199:
                 print("Epoch:", epoch, "Loss:", loss)
                 print("dw:", dw)
                 print("db:", db)
@@ -63,22 +64,43 @@ class LogisticRegressionCustom:
 
             if is_dp:
                 assert epsilon is not None and delta is not None
+                dw = X * dz.reshape(-1, 1) # grad of every sample
                 # Clip gradient
-                clip_gradients = clip_gradients(dw, C)
+                dw = clip_gradients(dw, C)
                 # Add noise to gradients
-                # TODO: Calculate epsilon_u, delta_u based epsilon, delta and epochs here.
-                epsilon_u, delta_u = None, None
+                # *-TODO: Calculate epsilon_u, delta_u based epsilon, delta and epochs here.
+                # 由Advanced Composition Theorem可知，对于每个epoch，我们可以将epsilon和delta分别除以epoch数，然后加到每个epoch上，这样就可以保证总的epsilon和delta不变。
+                k = self.num_iterations
+                # epsilon_u, delta_u = epsilon / k, delta / k
+                delta_u = delta / (k + 1)
+                # 近似计算epsilon_u
+                epsilon_u = epsilon / np.sqrt(2 * k * np.log(1 / delta_u))
+                # 下面式子中，分母中的epsilon_u大于真实的epsilon_u，因此可以保证epsilon_u的计算结果偏小，符合隐私保证
+                epsilon_u = epsilon / (np.sqrt(2 * k * np.log(1 / delta_u)) + k * (np.exp(epsilon_u) - 1)) 
                 dw = add_gaussian_noise_to_gradients(dw, epsilon_u, delta_u, C)
 
             # Update weights and bias
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
 
+
     def fit(self, X, y):
         self.__fit(X, y)
 
     def dp_fit(self, X, y, epsilon, delta, C):
         self.__fit(X, y, is_dp=True, epsilon=epsilon, delta=delta, C=C)
+        # 计算花费的隐私预算
+        k = self.num_iterations
+        # epsilon_u, delta_u = epsilon / k, delta / k
+        delta_u = delta / (k + 1)
+        # 近似计算epsilon_u
+        epsilon_u = epsilon / np.sqrt(2 * k * np.log(1 / delta_u))
+        # 下面式子中，分母中的epsilon_u大于真实的epsilon_u，因此可以保证epsilon_u的计算结果偏小，符合隐私保证
+        epsilon_u = epsilon / (np.sqrt(2 * k * np.log(1 / delta_u)) + k * (np.exp(epsilon_u) - 1))
+        epsilon_cost = np.sqrt(2 * k * np.log(1 / delta_u)) * epsilon_u + k * epsilon_u * (np.exp(epsilon_u) - 1)
+        delta_cost = delta_u * k + delta_u
+        print("epsilon_cost:", epsilon_cost)
+        print("delta_cost:", delta_cost)
 
     def predict_probability(self, X):
         linear_model = np.dot(X, self.weights) + self.bias
@@ -130,15 +152,24 @@ def get_train_data(dataset_name=None):
     return X_train, X_test, y_train, y_test
 
 
-def clip_gradients(gradients, C):
-    # TODO: Clip gradients.
-    clip_gradients = None
+def clip_gradients(sample_gradients, C):
+    # *-TODO: Clip gradients.
+    grad_norm = np.linalg.norm(sample_gradients, ord=2, axis=1)
+    clip_factor = np.maximum(1, grad_norm/C)
+    clip_gradients = sample_gradients / clip_factor[:, np.newaxis]
     return clip_gradients
 
 
-def add_gaussian_noise_to_gradients(gradients, epsilon, delta, C):
-    # TODO: add gaussian noise to gradients.
-    noisy_gradients = None
+def add_gaussian_noise_to_gradients(sample_gradients, epsilon, delta, C):
+    # *-TODO: add gaussian noise to gradients.
+    gradients = np.sum(sample_gradients, axis=0)
+
+    std = C * np.sqrt(2 * np.log(1.25 / delta)) / epsilon
+    noise = np.random.normal(loc=0.0, scale=std, size=gradients.shape)
+    noisy_gradients = gradients + noise
+
+    noisy_gradients = noisy_gradients / sample_gradients.shape[0]
+
     return noisy_gradients
 
 
@@ -148,15 +179,18 @@ if __name__ == "__main__":
     dataset_name = "cancer"
     X_train, X_test, y_train, y_test = get_train_data(dataset_name)
 
+    LEARNING_RATE = 0.01
+    NUM_ITERATIONS = 10000
+
     # Training the normal model
-    normal_model = LogisticRegressionCustom(learning_rate=0.01, num_iterations=1000)
+    normal_model = LogisticRegressionCustom(learning_rate=LEARNING_RATE, num_iterations=NUM_ITERATIONS)
     normal_model.fit(X_train, y_train)
     y_pred = normal_model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     print("Normal accuracy:", accuracy)
 
     # Training the differentially private model
-    dp_model = LogisticRegressionCustom(learning_rate=0.01, num_iterations=1000)
+    dp_model = LogisticRegressionCustom(learning_rate=LEARNING_RATE, num_iterations=NUM_ITERATIONS)
     epsilon, delta = 1.0, 1e-3
     dp_model.dp_fit(X_train, y_train, epsilon=epsilon, delta=delta, C=1)
     y_pred = dp_model.predict(X_test)
