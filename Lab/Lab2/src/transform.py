2662a3e44129cbb255052b15cd7a6a43f7816bb9
4332001876 <mzfslhm@163.com>
2024-01-21 22:40:25 +0800

    lab2 finished

--- Lab/Lab2/src/transform.py
+++ Lab/Lab2/src/transform.py
@@ -2,10 +2,10 @@ from sklearn import preprocessing
 
 
 def scale(dataset):
-    raw_dataset = dataset.get_dataset()
+    raw_dataset = dataset.get_dataset() # 获取数据集
 
-    start_col = 2 if dataset.has_label else 1
+    start_col = 2 if dataset.has_label else 1 # 从第2列开始是feature
     scaled_feats = preprocessing.scale(raw_dataset[:, start_col:], copy=False)
-    raw_dataset[:, start_col:] = scaled_feats
+    raw_dataset[:, start_col:] = scaled_feats # 对feature进行标准化
 
-    dataset.set_dataset(raw_dataset)
+    dataset.set_dataset(raw_dataset) # 更新数据集
